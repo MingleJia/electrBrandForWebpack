7b4cf3cd5276001a7a406ce9d69033a303a60f81
laihongbo@cnstrong.cn <laihongbo@cnstrong.cn>
2019-04-12 11:27:55 +0800

    校园风彩上下页切换修改

--- src/containers/campusStyle/campusDetail/index.jsx
+++ src/containers/campusStyle/campusDetail/index.jsx
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import styles from './index.scss';
 // import { defaultImg, } from 'ASSETS/campusstyle';
 import Tab from 'COMPONENTS/tab';
@@ -8,12 +8,13 @@ import PropTypes from 'prop-types';
 import axios from 'UTILS/axios';
 import moment from 'moment';
 
-class CampusDetail extends Component{
-    constructor(props){
+class CampusDetail extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             campusList: [], //所有校园风采列表
             compusContent: {}, //当前显示的校园风采信息
+            idx: 0
         }
     }
 
@@ -21,17 +22,21 @@ class CampusDetail extends Component{
         setCampusStyle: PropTypes.func,
         root: PropTypes.object,
     }
+    componentWillMount() {
+        this.setState({ idx: this.props.root.campusDetailId })
+    }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getContent();
     }
 
     getContent = () => {
-        axios('get','/api/campus/getList',{
-        }).then((json)=>{
-            this.setState({       
-                campusList : json.data,
-                compusContent: json.data[this.props.root.campusDetailId]
+        axios('get', '/api/campus/getList', {
+        }).then((json) => {
+            this.setState({
+                campusList: json.data,
+                // compusContent: json.data[this.props.root.campusDetailId]
+                compusContent: json.data[this.state.idx]
             })
 
         })
@@ -43,53 +48,67 @@ class CampusDetail extends Component{
     }
     //上一页
     prevBtn = () => {
-        let { campusDetailId } = this.props.root;
-        let { campusList } = this.state;
-        campusDetailId < 1 ? campusDetailId = campusList.length-1 : campusDetailId = campusDetailId - 1;
+        // let { campusDetailId } = this.props.root;
+        let { campusList, idx } = this.state;
+        idx < 1 ? idx = campusList.length - 1 : idx = idx - 1;
         this.setState({
-            compusContent: campusList[campusDetailId]
+            compusContent: campusList[idx],
+            idx,
         })
-        this.props.setCampusStyle({
-            campusDetailId: campusDetailId,
-        })
+        // this.props.setCampusStyle({
+        //     campusDetailId: campusDetailId,
+        // })
     }
     //下一页
     nextBtn = () => {
-        let { campusDetailId } = this.props.root;
-        let { campusList } = this.state;
-        campusDetailId >= campusList.length-1 ? campusDetailId = 0 : campusDetailId = campusDetailId + 1;
+        let { campusList, idx } = this.state;
+        idx >= campusList.length - 1 ? idx = 0 : idx = idx + 1;
         this.setState({
-            compusContent: campusList[campusDetailId]
-        })
-        this.props.setCampusStyle({
-            campusDetailId: campusDetailId,
+            compusContent: campusList[idx],
+            idx
         })
     }
-    render(){
-        const { compusContent } = this.state;
+    render() {
+        const { compusContent, campusList } = this.state;
+        console.log(this.state.idx);
         const campusDetail = (
             <div className={styles['container']}>
                 <div className={styles['tab']}>
-                    <div className={styles['back']} onClick={ ()=>this.backHome() }>
+                    <div className={styles['back']} onClick={() => this.backHome()}>
                         <span className={styles['backimg']}></span>
                         返回
                     </div>
-                    <div className={styles['btn']}>
-                        <span className={styles['prevBtn']} onClick={ ()=> this.prevBtn() }>上一个</span>
-                        <span className={styles['nextBtn']} onClick={ ()=> this.nextBtn() }>下一个</span>
-                    </div>
+                    {
+                        campusList.length < 2
+                            ?
+                            <div></div>
+                            :
+                            // <div className={styles['btn']}>
+                            //     <span className={styles['prevBtn']} onClick={() => this.prevBtn()}>上一个</span>
+                            //     <span className={styles['nextBtn']} onClick={() => this.nextBtn()}>下一个</span>
+                            // </div>
+                            <div className={styles['btnWrap']}>
+                                {
+                                    this.state.idx == 0 ? '' : <span className={styles['prevBtn']} onClick={() => this.prevBtn()}>上一个</span>
+                                }
+                                {
+
+                                    this.state.idx == campusList.length - 1 ? '' : <span className={styles['nextBtn']} onClick={() => this.nextBtn()}>下一个</span>
+                                }
+                            </div>
+                    }
                 </div>
                 <div className={styles['content']}>
                     <div className={styles['title']}>
-                        <span className={styles['titlename']}>{ compusContent.title }</span>
-                        <span className={styles['time']}>{ moment(compusContent.createtime).format('YYYY-MM-DD HH:mm') }</span>
+                        <span className={styles['titlename']}>{compusContent.title}</span>
+                        <span className={styles['time']}>{moment(compusContent.createtime).format('YYYY-MM-DD HH:mm')}</span>
                     </div>
                     <div className={styles['detail']}>
-                        <p className={styles['text']}>{ compusContent.content }</p>
+                        <p className={styles['text']}>{compusContent.content}</p>
                         {/* <img className={styles['picture']} src={defaultImg}/> */}
                     </div>
                 </div>
-                <Tab/>
+                <Tab />
             </div>
         )
         return campusDetail;
@@ -97,7 +116,7 @@ class CampusDetail extends Component{
 }
 
 export default connect(
-    ({ root} ) => ({
+    ({ root }) => ({
         root: root,
     }), { setCampusStyle }
 )(CampusDetail)
