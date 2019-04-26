139180ae53a37c96b25e36426e61e26c7be052a0
laihongbo@cnstrong.cn <laihongbo@cnstrong.cn>
2019-04-26 10:19:58 +0800

    编辑页面

--- src/containers/phoneStudentStyle/StudentsStyleEditP.jsx
+++ src/containers/phoneStudentStyle/StudentsStyleEditP.jsx
@@ -7,7 +7,10 @@ class StudentsStyleP extends Component {
         super(props);
         this.state = {
             show_time: new Date(Date.now()),
-            title: ''
+            title: '',
+            desc: '',
+            comment: '',
+            show_days: '',
         }
     }
 
@@ -17,7 +20,7 @@ class StudentsStyleP extends Component {
         this.setState({ [k]: v })
     }
     render() {
-        let { show_time, title } = this.state;
+        let { show_time, title, desc, comment, show_days } = this.state;
         // console.log(show_time)
         return <Fragment>
             <div className={styles['box']}>
@@ -55,7 +58,60 @@ class StudentsStyleP extends Component {
                         >标题</InputItem>
                     </div>
                 </div>
+                <div className={styles['textAreaWrap']}>
+                    <TextareaItem
+                        placeholder="请输入描述(选填)"
+                        autoHeight
+                        ref={el => this.customFocusInst = el}
+                        rows={6}
+                        count={200}
+                        value={desc}
+                        onChange={(v) => { this.setOneKV('desc', v) }}
+                    />
+                </div>
+                <div className={styles['teachersWrap']}>
+                    <div className={styles['row']}>
+                        <InputItem
+                            placeholder="请输入标题名称"
+                            ref={el => this.inputRef = el}
+                            extra={<div>{comment.length > 30 ? 30 : comment.length}/30</div>}
+                            value={comment}
+                            maxLength={30}
+                            onChange={(v) => { this.setOneKV('comment', v) }}
+                        >教师点评</InputItem>
+                    </div>
+                    <div className={styles['row']}>
+                        <Picker
+                            data={[
+                                {
+                                    label: '1天',
+                                    value: '1',
+                                },
+                                {
+                                    label: '2天',
+                                    value: '2',
+                                },
+                                {
+                                    label: '3天',
+                                    value: '3',
+                                },
+                                {
+                                    label: '4天',
+                                    value: '4',
+                                },
+                            ]}
+                            cols={1}
+                            value={show_days}
+                            onChange={(v) => { this.setOneKV('show_days', v) }}
+                        >
+                            <List.Item arrow="horizontal">展示天数</List.Item>
+                        </Picker>
+                    </div>
+                </div>
                 <UploadImgs />
+                <div className={styles['btn']}>
+                    确定
+                </div>
             </div>
         </Fragment >
     }
