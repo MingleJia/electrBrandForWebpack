b185ffdd70327836f64399055720cfc28007d63e
laihongbo@cnstrong.cn <laihongbo@cnstrong.cn>
2019-04-26 09:15:31 +0800

    编辑页面样式

--- src/containers/phoneStudentStyle/StudentsStyleEditP.jsx
+++ src/containers/phoneStudentStyle/StudentsStyleEditP.jsx
@@ -17,7 +17,7 @@ class StudentsStyleP extends Component {
     }
     render() {
         let { show_time, title } = this.state;
-        console.log(show_time)
+        // console.log(show_time)
         return <Fragment>
             <div className={styles['box']}>
                 <div className={styles['top']}>
@@ -44,24 +44,14 @@ class StudentsStyleP extends Component {
                         </DatePicker>
                     </div>
                     <div className={styles['row']}>
-                        {/* <div className={styles['title']}>
-                            标题
-                        </div> */}
-                        <div className={styles['textareaWrap']}>
-                            {/* <TextareaItem
-                                placeholder={'请输入标题'}
-                                rows={1}
-                                count={30}
-                                value={title}
-                                onChange={(v) => { this.setOneKV('title', v) }}
-                            /> */}
-                            <InputItem
-                                clear
-                                placeholder="click the button below to focus"
-                                ref={el => this.inputRef = el}
-                            >标题</InputItem>
-                            <div className={styles['count']}>{title.length > 30 ? 30 : title.length}/30</div>
-                        </div>
+                        <InputItem
+                            placeholder="请输入标题名称"
+                            ref={el => this.inputRef = el}
+                            extra={<div>{title.length > 30 ? 30 : title.length}/30</div>}
+                            value={title}
+                            maxLength={30}
+                            onChange={(v) => { this.setOneKV('title', v) }}
+                        >标题</InputItem>
                     </div>
                 </div>
             </div>
